4027e0a2669bc405a780b34f9663bd448f8df5db
Curtis Castro <curtisncastro@gmail.com>
2021-12-05 13:09:00 -0800

    Conditional operators

--- tut3.cpp
+++ tut3.cpp
@@ -1,9 +1,36 @@
 #include "cstdlib"
 #include "iostream"
 #include "string"
-
 using namespace std;
 
 int main() {
+  string sQuestion ("Enter Number 1: ");
+  string sNum1, sNum2;
+  cout << sQuestion;
+  cin >> sNum1;
+  cout << "Enter Number 2: ";
+  cin >> sNum2;
+  int nNum1 = stoi(sNum1);
+  int nNum2 = stoi(sNum2);
 
+  printf("%d + %d = %d\n", nNum1, nNum2, (nNum1 + nNum2));
+  printf("%d - %d = %d\n", nNum1, nNum2, (nNum1 - nNum2));
+  printf("%d * %d = %d\n", nNum1, nNum2, (nNum1 * nNum2));
+  printf("%d / %d = %d\n", nNum1, nNum2, (nNum1 / nNum2));
+  printf("%d %% %d = %d\n", nNum1, nNum2, (nNum1 % nNum2));
+
+// PROBLEM
+// Enter Miles: 5
+// 5 Miles equals 8.0467 kilometers
+
+  string sMiles;
+  double dMiles, dKilometers;
+  cout << "Enter Miles: ";
+  cin >> sMiles;
+  dMiles = stod(sMiles);
+  dKilometers = dMiles * 1.60934;
+
+  printf("%.1f miles equals %.4f kilometers\nf", dMiles, dKilometers);
+
+  return 0;
 }
---
+++ tut4.cpp
@@ -0,0 +1,43 @@
+#include "cstdlib"
+#include "iostream"
+#include "string"
+using namespace std;
+
+int main() {
+  // CONDITIONAL OPERATORS
+  // ==, !=, <, >, <=, >=
+
+  // LOGICAL OPERATORS
+  // && : If both are true, returns true
+  // || : If either are true, returns true
+  // !  : Converts true into false and vice versa
+
+  // Make a program that decides if a birthday is important or not
+  // 1-18 are important
+  // 21 is important
+  // 21-50 not important
+  // over 65 is important
+
+
+  // PROBLEM: Determine School Grade
+  // If 5 "Go to Kindergarten"
+  // 6-17 go to grades 1-12
+  // 18+ "Go to college"
+  // under 5: too young for school
+  string sAge;
+  cout << "Enter Age: ";
+  cin >> sAge;
+  int nAge = stoi(sAge);
+  int nGrade = nAge - 5;
+  if (nAge < 5) {
+    cout << "Too Young For School\n";
+  } else if (nAge == 5) {
+    cout << "Go to Kindergarten\n";
+  } else if (nAge >= 6 && nAge <= 17) {
+    printf("Go to Grade %d\n", nGrade);
+  } else {
+    cout << "Go to College\n";
+  }
+
+  return 0;
+}
